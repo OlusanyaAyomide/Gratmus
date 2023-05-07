8e6090090380909c4d322ef3ba4dcb8a4bd03203
Olusanya Ayomide <ayomideflex72@gmail.com>
2023-05-07 20:51:10 +0100

    profile-commit

--- public/company/CompanyExporter.tsx
+++ public/company/CompanyExporter.tsx
@@ -6,6 +6,10 @@ import Bulb from "./Bulb.png";
 import Rectangle3 from "./Rectangle3.png";
 import Logo from "./Logo.svg";
 import Medal from "./Medal.png";
+import Dancing from "./Dancing.svg";
+import Group from "./Agroup.png";
+import Search from "./Aserach.png";
+import Star from "./Astar.png"
 export const CompanyExporter = {
-    HeroGirl,Man,Rectangle1,Rectangle2,Bulb,Rectangle3,Logo,Medal
+    HeroGirl,Man,Rectangle1,Rectangle2,Bulb,Rectangle3,Logo,Medal,Dancing,Group,Search,Star
 }
